2424ef44d1e084f277a67a4b566fbf7386c51ba7
sezan03 <49815431+sezan03@users.noreply.github.com>
2019-04-20 21:07:25 +0600

    Rooftop Water System

---
+++ ard_01/ard_01.ino
@@ -0,0 +1,47 @@
+int trigPin = 10;
+int echoPin = 9;
+int led = 7;
+int red_led = 6;
+int pump = 5;
+int emergency_pin = 13;
+
+
+void setup() {
+  Serial.begin(9600); 
+   pinMode(led, OUTPUT);
+   pinMode(red_led, OUTPUT);
+   pinMode(pump, OUTPUT);
+   pinMode(trigPin, OUTPUT);
+   pinMode(echoPin, INPUT);
+   pinMode(emergency_pin, INPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin, LOW);
+  duration=pulseIn(echoPin, HIGH);
+  distance =(duration/2)/29.1;
+  Serial.print(distance);
+  Serial.println("CM");
+  delay(10);
+ 
+ 
+ 
+ if((distance>=12)) 
+  {
+    digitalWrite(led, HIGH);
+    digitalWrite(pump, HIGH);
+    digitalWrite(red_led, LOW);
+}
+   else if(distance<=3)
+ {
+     digitalWrite(led, LOW);
+     digitalWrite(pump, LOW);
+     digitalWrite(red_led, HIGH);
+   }
+ 
+}
---
+++ pump_02/pump_02.ino
@@ -0,0 +1,28 @@
+int out = 13; 
+int digital_input = 8; 
+int green_led = 7;
+int red_led = 6;
+int val; 
+void setup() {
+  pinMode(13,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(6,OUTPUT); //Set pin 13 as OUTPUT pin, to send signal to relay
+  pinMode(8,INPUT); //Set pin 8 as input pin, to receive data from Soil moisture sensor.
+}
+
+void loop() { 
+  val = digitalRead(8); 
+  if(val == LOW) 
+  {
+  digitalWrite(13,LOW); 
+  digitalWrite(7,LOW); 
+  digitalWrite(6,HIGH);
+  }
+  else
+  {
+  digitalWrite(13,HIGH); 
+  digitalWrite(7,HIGH);
+  digitalWrite(6,LOW);
+  }
+  delay(400);
+}
